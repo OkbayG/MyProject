b22f2d009282532e438e981aa1ab0096282a1fca
OkbayG <okbay.goitom@stud.kftg.ch>
2021-04-01 18:27:16 +0200

    hffdthr

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+  error_reporting(0);
+  readfile('index_header.html');
+  readfile('register.html');
+  readfile('index_footer.html');
---
+++ login.php
@@ -0,0 +1,5 @@
+<?php
+  error_reporting(0);
+  readfile('index_header.html');
+  readfile('login.html');
+  readfile('index_footer.html');
